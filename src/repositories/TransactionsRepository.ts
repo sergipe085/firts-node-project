ca4b96c12f8cc2407f8bf8cac2870a6673274470
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-12 20:54:47 -0300

    final commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string,
+  value: number,
+  type: 'income' | 'outcome',
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,31 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce((accumulator: Balance, transaction: Transaction) => {
+
+      accumulator.income += transaction.type == 'income' ? transaction.value : 0;
+      accumulator.outcome += transaction.type == 'outcome' ? transaction.value : 0;
+
+      return accumulator;
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    });
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
